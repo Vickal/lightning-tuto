7bd8fabde0ac80e356f7e7a0cc70cd6a82cad793
Vickal <vickalgupta@zencloudtech.com>
2018-08-09 11:56:34 +0530

    removed old files.

--- ltng-CustomAccordion/aura/Lightning_CustomAccordionDemo/Lightning_CustomAccordionDemoController.js
+++
@@ -1,8 +0,0 @@
-({
-	doInit : function(component, event, helper) {
-		helper.getContacts(component, event);
-	},
-	doExpandAll : function(component, event, helper){
-       helper.doFireExpandAllEvent(component, event);
-    },
-})
--- ltng-CustomAccordion/aura/Lightning_RichCustomAccordion/Lightning_RichCustomAccordionController.js
+++
@@ -1,9 +0,0 @@
-({
-	doToggle : function(component, event, helper) {
-		helper.toggleCollapseHandler(component, event);
-	},
-
-	doExpandAll: function(component,event,helper){
-        helper.doHandleExpandAll(component,event);
-    }
-})
